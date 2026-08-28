00c66774ae995198c7265f20a6baaad76da57860
taliakasissieh <talia.kasissieh@gmail.com>
2026-08-28 11:37:58 +0300

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.cancer-insight.com',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ];
+}
