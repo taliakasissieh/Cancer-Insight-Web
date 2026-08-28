5c2b3aaa86abc2f76bbee21aaeeb712dc6c3b60e
taliakasissieh <talia.kasissieh@gmail.com>
2026-08-28 11:36:26 +0300

    Create robots.js

---
+++ app/robots.js
@@ -0,0 +1,10 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+
+    sitemap: 'https://www.cancer-insight.com/sitemap.xml',
+  };
+}
