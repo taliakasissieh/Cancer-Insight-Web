b0d6e33803e09096b0869acbf80f02d55bce4fc1
taliakasissieh <talia.kasissieh@gmail.com>
2026-09-17 18:17:26 +0300

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,20 +1,48 @@
 export default function sitemap() {
+  const baseUrl =
+    'https://www.cancer-insight.com';
+
   return [
     {
-      url: 'https://www.cancer-insight.com/',
+      url: `${baseUrl}/`,
       lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
     },
+
     {
-      url: 'https://www.cancer-insight.com/privacy-policy',
+      url: `${baseUrl}/methodology`,
       lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
     },
+
     {
-      url: 'https://www.cancer-insight.com/contact',
+      url: `${baseUrl}/faq`,
       lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
     },
+
     {
-      url: 'https://www.cancer-insight.com/terms',
+      url: `${baseUrl}/privacy-policy`,
       lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.4,
+    },
+
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+
+    {
+      url: `${baseUrl}/terms`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.4,
     },
   ];
 }
